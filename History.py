4e6b9414f0bdc7f209ad6327da873797d92b1564
SPHornsby <sean.hornsby@gmail.com>
2023-05-13 19:30:25 -0700

    remove unused history method. Adds error handling.

--- History.py
+++ History.py
@@ -4,22 +4,19 @@ class History:
     def __init__(self, fileName = None):
         self.__chat = []
         self.__fileName = fileName
-        if fileName != None and os.path.isfile('./' + fileName):
-            fh = open(fileName, 'r')
-            for line in fh:
-                self.addToHistory(line)
-            fh.close()
 
     def addToHistory(self, line):
-        self.__chat.append(line)
-    
-    def readHistory(self, number_of_lines=0):
-        if number_of_lines == 0:
-            for line in self.__chat:
-                print(line+'\n')
-    
+        try:
+            self.__chat.append(line)
+        except:
+            print('There was a problem storing the last line. The chat transcript may not be complete.')
+
     def saveHistory(self):
-        fh = open(self.__fileName, 'a')
-        for line in self.__chat:
-            fh.write(line + '\n')
-        fh.close()
+        try:
+            fh = open(self.__fileName, 'a')
+            for line in self.__chat:
+                fh.write(line + '\n')
+            fh.close()
+        except Exception as e:
+            print('There was an problem saving the file.')
+            exit(1)
