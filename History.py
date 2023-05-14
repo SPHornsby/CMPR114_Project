ee10d192233eb4815cae8c3586d3ed189f3ee1b2
SPHornsby <sean.hornsby@gmail.com>
2023-05-14 12:09:33 -0700

    Documentation and comments

--- History.py
+++ History.py
@@ -1,6 +1,7 @@
 import os
 
 class History:
+    '''This class deals with storing the chat history in local memory and then persisting it to a file. '''
     def __init__(self, fileName = None):
         self.__chat = []
         self.__fileName = fileName
@@ -11,11 +12,14 @@ class History:
         except:
             print('There was a problem storing the last line. The chat transcript may not be complete.')
 
-    def saveHistory(self):
+    def saveHistory(self)->None:
         try:
             fh = open(self.__fileName, 'a')
             for line in self.__chat:
-                line = convertTuple(line)
+                if type(line) is tuple:
+                    line = convertTuple(line)
+                if type(line) is not str:
+                    raise ValueError('Chat could not be converted to string')
                 fh.write(line + '\n')
             fh.close()
         except Exception as err:
@@ -23,7 +27,8 @@ class History:
             print('There was a problem saving the file.')
             exit(1)
 
-def convertTuple(tup):
+def convertTuple(tup:tuple)->str:
+    '''Converts a tuple into a concatenated string'''
     # initialize an empty string
     str = ''
     for item in tup:
--- main.py
+++ main.py
@@ -5,25 +5,35 @@ from chatmodels import inputs as iomodels
 # import History as history
 def main():
     try:
+        # initiate loop
         loop = True
+        # set defaults
         save_chat = False
         model = 1
-        # print(iomodels)
+
+        # show the model choices
         for m, n in iomodels.items():
             print(f'{n}. {m}')
-            # print(m)
-            # pass
+        # allow the user to pick a model (or not)
         model = int(input('Choose a completion model (or leave blank for default model '))
+        # quick and dirty method to make sure the model is in the choices
         if model < 1 or model > 5:
             model = 1
+        
+        # ask if user wants to save the chat as a file
         if input('Would you like to store the chat history? (y/n) ') == 'y':
             save_chat = True
             file_name = input('What is the name of the file where you want to save the history? ')
+            # force filename to end in .txt so it is saved as a text file
             if not file_name.endswith('.txt'):
                 file_name = file_name + '.txt'
+            # instantiate the chat with all of the choices.
             c = chat.Chat(model = model, with_history = save_chat, file_name = file_name)
         else:
-            c = chat.Chat()
+            # taske the default, no history chat
+            c = chat.Chat(model = model)
+
+        # start asking questions and getting answers
         while loop:
             question = input('What question would you like to ask? (Leave blank to quit)')
             if question == '':
