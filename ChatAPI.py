99bbbdb978f695f54669f15228046cc1db76a2ea
SPHornsby <sean.hornsby@gmail.com>
2023-05-14 20:37:22 -0700

    add error handling to chat

--- ChatAPI.py
+++ ChatAPI.py
@@ -10,36 +10,42 @@ from chatmodels import chats as cm
 load_dotenv()
 COMPLETION_URL = 'https://api.openai.com/v1/chat/completions'
 # MODEL='text-davinci-003'
-openai.organization = "org-aDyo6jEbWJVp1yvaaDJqlcwU"
+openai.organization =  os.getenv("OPENAI_API_ORG")#"org-aDyo6jEbWJVp1yvaaDJqlcwU"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class Chat:
+    '''Chat wraps the openai library to provide access to the chatCompletion methods.'''
     def __init__(self, model = 1, with_history = False, file_name = 'default.txt'):
         self.__model = cm[model]
         self.__history = None
         if with_history:
             self.__history = history.History(file_name)
 
-    def chat(self, question):
-        start = time.time()
-        response =openai.ChatCompletion.create(
-            model=self.__model,
-            messages=[
-                {'role':'user', 'content':question}
-            ]
-        )
-        end = time.time()
+    def chat(self, question:str)->str:
+        try:
+            start = time.time()
+            response =openai.ChatCompletion.create(
+                model=self.__model,
+                messages=[
+                    {'role':'user', 'content':question}
+                ]
+            )
+            end = time.time()
 
-        usage = response['usage']
-        total_tokens = usage['total_tokens']
-        print(f'Total tokens used for this question: {total_tokens}')
-        response = response['choices'][0]['message']['content']
-        if not self.__history == None:
-            self.__history.addToHistory((str(start),': Question: '+ question.strip()))
-            self.__history.addToHistory((str(end),': Response: '+ response.strip(), ', response time: ' + str(round(end - start, 2))))
-            self.__history.addToHistory('total tokens used: '+str(total_tokens))
-        return response
-    
+            usage = response['usage']
+            total_tokens = usage['total_tokens']
+            print(f'Total tokens used for this question: {total_tokens}')
+            response = response['choices'][0]['message']['content']
+            if not self.__history == None:
+                self.__history.addToHistory((str(start),': Question: '+ question.strip()))
+                self.__history.addToHistory((str(end),': Response: '+ response.strip(), ', response time: ' + str(round(end - start, 2))))
+                self.__history.addToHistory('total tokens used: '+str(total_tokens))
+            return response
+        except openai.error.RateLimitError as err:
+            time.sleep(10)
+            return 'The question could not be asked because the rate limit was exceeded. Please try again.'
+        except:
+            raise ChatException
     def ask(self, question):
         try:
             start = time.time()
